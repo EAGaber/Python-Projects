d9c956eb9957f09541c295095427ee4539c54a86
EAGaber <emilygaber344@gmail.com>
2026-09-23 14:02:56 -0500

    Create Great.py

---
+++ Great.py
@@ -0,0 +1,11 @@
+# Author: Emily Gaber
+# Date Created; 9/23/2026
+# Date Updated: 9/23/2026
+# Purpose: TO tell you you're great!
+
+
+
+Greatness = 100
+x = 100
+if (x = Great)
+ print("You're Great!")
