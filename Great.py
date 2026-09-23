66d292d8291066b3278af35451b4e57833d83f6e
EAGaber <emilygaber344@gmail.com>
2026-09-23 14:13:07 -0500

    Start

--- Great.py
+++ Great.py
@@ -1,11 +1,14 @@
 # Author: Emily Gaber
 # Date Created; 9/23/2026
 # Date Updated: 9/23/2026
-# Purpose: TO tell you you're great!
+# Purpose: TO tell you YOU'RE great!
 
 
 
 Greatness = 100
 x = 100
-if (x = Great)
+if x = Great
  print("You're Great!")
+
+    if else x != Great
+    print("That's okay I still think you are.")
